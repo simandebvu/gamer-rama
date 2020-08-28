f6038a212a2c89fcb7c00ab82c00110903d4303d
Shingirayi Innocent Mandebvu <shingim1@gmail.com>
2020-08-29 00:28:17 +0200

    style: center options menu items

--- src/modules/Scenes/OptionsScene.js
+++ src/modules/Scenes/OptionsScene.js
@@ -9,9 +9,9 @@ export default class OptionsScene extends Phaser.Scene {
   create() {
     this.model = this.sys.game.globals.model;
 
-    this.text = this.add.text(300, 100, 'Options', { fontSize: 40 });
-    this.musicButton = this.add.image(200, 200, 'checkedBox');
-    this.musicText = this.add.text(250, 190, 'Music Enabled', { fontSize: 24 });
+    this.text = this.add.text(150, 100, 'Options', { fontSize: 40 });
+    this.musicButton = this.add.image(130, 200, 'checkedBox');
+    this.musicText = this.add.text(160, 190, 'Music Enabled', { fontSize: 24 });
 
     this.musicButton.setInteractive();
 
@@ -20,7 +20,7 @@ export default class OptionsScene extends Phaser.Scene {
       this.updateAudio();
     });
 
-    this.menuButton = new Button(this, 400, 500, 'blueButton1', 'blueButton2', 'Menu', 'Menu');
+    this.menuButton = new Button(this, 230, 400, 'blueButton1', 'blueButton2', 'Menu', 'Menu');
 
     this.updateAudio();
   }
