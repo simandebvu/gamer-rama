44e7e6a572a518f3650c1e688beb49d9b085e7a6
Shingirayi Innocent Mandebvu <shingim1@gmail.com>
2020-08-28 21:50:53 +0200

    refactor: add scenes in index

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 import Phaser from 'phaser';
+import Model from './modules/Objects/ModelObject';
 import config from './modules/Config/config';
 import bootScene from './modules/Scenes/BootScene';
 import preloaderScene from './modules/Scenes/PreloaderScene';
 import menuScene from './modules/Scenes/MenuScene';
 import gameScene from './modules/Scenes/GameScene';
 import gameOverScene from './modules/Scenes/GameOverScene';
+import optionsScene from './modules/Scenes/OptionsScene';
+import creditsScene from './modules/Scenes/CreditsScene';
 import './assets/styles.css';
 
 class Game extends Phaser.Game {
   constructor() {
     super(config);
+    const model = new Model();
+    this.globals = { model, bgMusic: null };
     this.scene.add('Boot', bootScene);
     this.scene.add('Preloader', preloaderScene);
     this.scene.add('Menu', menuScene);
+    this.scene.add('Options', optionsScene);
+    this.scene.add('Credits', creditsScene);
     this.scene.add('Game', gameScene);
     this.scene.add('GameOver', gameOverScene);
     this.scene.start('Boot');
