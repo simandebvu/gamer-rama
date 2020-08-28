2ecc94262c72e0d68103508138684878af2d43fb
Shingirayi Innocent Mandebvu <shingim1@gmail.com>
2020-08-28 18:47:39 +0200

    style: add animation on menu scene

--- src/modules/Scenes/MenuScene.js
+++ src/modules/Scenes/MenuScene.js
@@ -1,6 +1,7 @@
 import Phaser from 'phaser';
 import config from '../Config/config';
 import Button from '../Objects/ButtonObject';
+import ScrollingBackground from '../Entities/ScrollingBackgroundEntity';
 
 export default class MenuScene extends Phaser.Scene {
   constructor() {
@@ -8,16 +9,26 @@ export default class MenuScene extends Phaser.Scene {
   }
 
   preload() {
+    this.load.image('sprBg0', 'assets/images/sprBg0.png');
+    this.load.image('sprBg1', 'assets/images/sprBg1.png');
     this.load.image('gameImage', 'assets/images/cover.png');
   }
 
   create() {
+    this.backgrounds = [];
+    for (let i = 0; i < 5; i += 1) {
+      const keys = ['sprBg0', 'sprBg1'];
+      const key = keys[Phaser.Math.Between(0, keys.length - 1)];
+      const bg = new ScrollingBackground(this, key, i * 10);
+      this.backgrounds.push(bg);
+    }
     this.textConfig = {
       color: '#0277bd',
       fontFamily: 'sans-serif',
       fontSize: '30px',
       lineHeight: 1.3,
-      align: 'justify',
+      align: 'center',
+      fontStyle: 'bold',
       wordWrap: {
         width: this.game.config.width * 0.5,
         useAdvancedWrap: true,
@@ -38,5 +49,11 @@ export default class MenuScene extends Phaser.Scene {
     this.optionsButton = new Button(this, config.width / 2, config.height / 2, 'blueButton1', 'blueButton2', 'Options', 'Options');
 
     this.creditsButton = new Button(this, config.width / 2, config.height / 2 + 100, 'blueButton1', 'blueButton2', 'Credits', 'Credits');
+  }
+
+  update() {
+    for (let i = 0; i < this.backgrounds.length; i += 1) {
+      this.backgrounds[i].update();
+    }
   }
 }
