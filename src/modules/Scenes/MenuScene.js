b8f264c9ed19db86ef2cbf1f22ddaa0c4c4e711c
Shingirayi Innocent Mandebvu <shingim1@gmail.com>
2020-08-28 21:51:10 +0200

    feat: add music option

--- src/modules/Scenes/MenuScene.js
+++ src/modules/Scenes/MenuScene.js
@@ -49,6 +49,14 @@ export default class MenuScene extends Phaser.Scene {
     this.optionsButton = new Button(this, config.width / 2, config.height / 2, 'blueButton1', 'blueButton2', 'Options', 'Options');
 
     this.creditsButton = new Button(this, config.width / 2, config.height / 2 + 100, 'blueButton1', 'blueButton2', 'Credits', 'Credits');
+
+    this.model = this.sys.game.globals.model;
+    if (this.model.musicOn === true && this.model.bgMusicPlaying === false) {
+      this.bgMusic = this.sound.add('bgMusic', { volume: 0.5, loop: true });
+      this.bgMusic.play();
+      this.model.bgMusicPlaying = true;
+      this.sys.game.globals.bgMusic = this.bgMusic;
+    }
   }
 
   update() {
